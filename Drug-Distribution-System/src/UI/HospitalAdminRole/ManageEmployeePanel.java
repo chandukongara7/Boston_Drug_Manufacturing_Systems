2096d60ba983fd0b1c343370a74b4d70d84ade05
Junaid Patel <patel.juna@northeastern.edu>
2022-12-10 02:01:16 -0500

    ui pharmacy role

--- Drug-Distribution-System/src/UI/HospitalAdminRole/ManageEmployeePanel.java
+++ Drug-Distribution-System/src/UI/HospitalAdminRole/ManageEmployeePanel.java
@@ -156,7 +156,7 @@ public class ManageEmployeePanel extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 47, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 286, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblOrganisationFilter)
                     .addComponent(cmbOrganisationFilter, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
