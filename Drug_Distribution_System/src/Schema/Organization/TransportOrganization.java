bc6937bb99c358e453c783b1f3b0cee4c089f994
LAKSHYA GUPTA <gupta.la@northeastern.edu>
2022-12-11 00:57:51 -0500

    Confict resolution

---
+++ Drug_Distribution_System/src/Schema/Organization/TransportOrganization.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Schema.Organization;
+
+import Schema.Role.Role;
+import Schema.Role.TransportationRole;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ganes
+ */
+public class TransportOrganization extends Organization {
+public TransportOrganization(){
+super(Organization.OrganizationType.Transport.getValue());
+    
+}
+@Override
+public ArrayList<Role> getSupportedRole(){
+    ArrayList<Role> roles = new ArrayList<>();
+    roles.add(new TransportationRole());
+    return roles;
+}
+}
