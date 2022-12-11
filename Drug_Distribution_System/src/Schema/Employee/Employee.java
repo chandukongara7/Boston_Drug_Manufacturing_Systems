7410b6363d1c97d84a7ab02b65936b2ff98ac2b5
Poornachandraprasad Kongara <kongara.p@northeastern.edu>
2022-12-11 02:28:27 -0500

    Resolved Conflicts

---
+++ Drug_Distribution_System/src/Schema/Employee/Employee.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Schema.Employee;
+
+/**
+ *
+ * @author ganes
+ */
+public class Employee {
+    private String EmployeeName;
+    private int EmployeeId;
+    private static int count = 1;
+    
+    public Employee() {
+        EmployeeId = count;
+        count++;
+    }
+    public int getEmployeeId(){
+        return EmployeeId;
+    }
+    public void setName (String EmployeeName){
+        this.EmployeeName=EmployeeName;
+        }
+    public String getEmployeeName(){
+        return EmployeeName;
+        }
+    @Override
+    public String toString(){
+        return EmployeeName;
+    }
+}
---
+++ Drug_Distribution_System/src/Schema/Organization/AdminOrg.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Schema.Organization;
+
+import Schema.Role.AdminRole;
+import Schema.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author krishna
+ */
+public class AdminOrg extends Organization {
+    private String organizationName;
+
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+    
+    public AdminOrg(){
+        super(OrganizationType.Admin.getValue());
+    }
+ 
+    
+    
+    @Override
+    public ArrayList<Role> getSupportedRole(){
+        ArrayList<Role> roles = new ArrayList<>();
+        roles.add(new AdminRole());
+        return roles;
+    }
+}
